68713e962525e3eb41e834ee4aefb81fae753307
Dario Steven Henao Salazar <dshenao94@misena.edu.co>
2023-08-13 15:32:00 -0500

    configuracion

---
+++ ProjectQT/Data/InfoMaquina.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectQT.Data
+{
+    public class InfoMaquina
+    {
+        public int Nivel { get; set; }
+        public int Material { get; set; }
+    }
+}
---
+++ ProjectQT/Data/InfoPaqueteGemas.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectQT.Data
+{
+    public class InfoPaqueteGemas
+    {
+        public int Maximo { get; set; }
+        public int CostoGemas { get; set; }
+        public int CantidadMaterial { get; set; }
+    }
+}
